4686b6fab1dd7cedd9fc5086e8d49f6b1f4cc66a
UMASK-2326 <umaharibabu2003@gmail.com>
2025-01-24 19:37:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template
+import os
 import random
 
 app = Flask(__name__)
 
-# list of cat images
-images = [
-    "https://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr01/15/9/anigif_enhanced-buzz-31540-1381844535-8.gif?downsize=715:*&output-format=auto&output-quality=auto",
-    "https://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26388-1381844103-11.gif?downsize=715:*&output-format=auto&output-quality=auto"
-]
+# Path to the images directory
+IMAGE_FOLDER = "images/uma.jpg"
+
+# List of image filenames from the folder
+images = [f for f in os.listdir(IMAGE_FOLDER) if f.endswith(('png', 'jpg', 'jpeg', 'gif'))]
 
 @app.route('/')
 def index():
-    url = random.choice(images)
-    return render_template('index.html', url=url)
+    if images:
+        image_path = os.path.join(IMAGE_FOLDER, random.choice(images))  # Select a random image
+    else:
+        image_path = None  # No images found
+
+    return render_template('index.html', image_path=image_path)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
