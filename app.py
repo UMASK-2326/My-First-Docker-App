fdfcbe8c4bbd6faa91d8f6f6e6bc149c43951dca
uma haribabu <umaharibabu2003@gmail.com>
2025-01-24 20:15:03 +0530

    output varum

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', image_path="images/uma.jpg")  # Change to your image name
+    return render_template('index.html', image_path="static/images/uma.jpg")  # Change to your image name
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
