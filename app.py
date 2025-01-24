a7efd65e107458e513cdfe0669d081d52f270900
UMASK-2326 <umaharibabu2003@gmail.com>
2025-01-24 20:33:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
-    image_path = url_for('static', filename='images/uma.jpg')  # Correct way to reference static files
+    image_path = url_for('static', filename='images/uma.jpg')  # Correctly reference static files
     return render_template('index.html', image_path=image_path)
 
 if __name__ == "__main__":
